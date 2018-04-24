c473cf00a788ad867d3ccc6de6c56251fcf3e3c3
Emmanuel BORGES <contact@eborges.fr>
2018-04-24 20:05:40 +0200

    configure aws for db

---
+++ src/Provider/ProviderInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manu
+ * Date: 24/04/2018
+ * Time: 19:33
+ */
+
+namespace App\Provider;
+
+
+interface ProviderInterface
+{
+
+}
---
+++ src/Provider/WhoscoredProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manu
+ * Date: 24/04/2018
+ * Time: 19:32
+ */
+
+namespace App\Provider;
+
+
+class WhoscoredProvider implements ProviderInterface
+{
+
+}
